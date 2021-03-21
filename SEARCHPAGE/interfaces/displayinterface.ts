a91db20b3ffb40ac86ec0775dee759ea7264c223
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-03-21 19:05:42 +0530

    Create displayinterface.ts

---
+++ SEARCHPAGE/interfaces/displayinterface.ts
@@ -0,0 +1,11 @@
+export interface Displayinterface {
+    customername:string;
+    customerid:string;
+    email:string;
+    age:number;
+    country:string;
+    city:string;
+    phone:number;
+    product:string;
+    
+}
